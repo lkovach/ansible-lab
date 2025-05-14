4a464534dab42c9c14a0507cc4cbcc5c5cf83902
Louis Kovach <louis.kovach149@gmail.com>
2025-05-14 08:29:57 -0400

    corrected a KB number

--- check_win_updates.py
+++ check_win_updates.py
@@ -9,7 +9,7 @@ import platform
 # Configure logging
 logging.basicConfig(filename="update_checker.log", level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
-KB_LIST = ["KB5058392", "KB5058385", "KB5058383", "KB5058392"]  # UPDATED 5-14-25 for May Patch Tuesday
+KB_LIST = ["KB5058524", "KB5058385", "KB5058383", "KB5058392"]  # UPDATED 5-14-25 for May Patch Tuesday
 ANSIBLE_CONTROLLER_PATH = "/opt/ansible/win_updates"
 INDIVIDUAL_RESULTS_FILE = os.path.join(ANSIBLE_CONTROLLER_PATH, f"{socket.gethostname()}_updates.csv")
 AGGREGATED_FILE = os.path.join(ANSIBLE_CONTROLLER_PATH, "aggregated_updates.csv")
