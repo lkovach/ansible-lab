5e26957b21ef04ec16e293a9537f8a0adf641a81
Louis Kovach <louis.kovach149@gmail.com>
2025-07-18 21:10:28 -0400

    Create new script using HTML

---
+++ windows_updates/check_updates_ps_html.ps1
@@ -0,0 +1,25 @@
+# Check for specific Windows updates using PowerShell
+$KBsToCheck = @("KB5062572", "KB5062557", "KB5062799", "KB5062560", "KB5062570")
+
+$htmlHead = @"
+<html><body><table border="1" cellpadding=4>
+<tr><th>KB Article</th><th>Installed</th><th>Host</th><th>Domain</th><th>IP Address</th><th>OS Version</th></tr>
+"@
+
+$htmlRows = $report | ForEach-Object {
+    $bg = $_.Installed -eq "Yes" ? '#90EE90' : '#FA8072'
+    "<tr>" +
+      "<td>$($_.KB_Article)</td>" +
+      "<td style='background-color:$bg'>$($_.Installed)</td>" +
+      "<td>$($_.Hostname)</td>" +
+      "<td>$($_.Domain)</td>" +
+      "<td>$($_.IP_Address)</td>" +
+      "<td>$($_.OS_Version)</td>" +
+    "</tr>"
+}
+
+$htmlFoot = "</table></body></html>"
+($htmlBody = $htmlRows + $htmlFoot) |
+  Out-File C:\Updates\patch_report_$env:COMPUTERNAME.html -Encoding UTF8
+
+Write-Host "HTML report created: C:\Updates\patch_report_$env:COMPUTERNAME.html"
